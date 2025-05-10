3a4500717b8cfa7f5a8ded7a8ee29e7bdff8ad0b
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-10 19:47:09 -0300

    feat: create terraform provider

---
+++ 0-provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0-beta1"
+    }
+     random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
